ed8c3a7560b203401e0bba38a057f9d4ef840cee
JOEL-BINU-PHILIP <joelbinuphilip@gmail.com>
2025-12-14 17:30:31 +0530

    Added Auth Guard and Config files

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { TokenStorageService } from '../services/token-storage.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const tokenStorage = inject(TokenStorageService);
+  const router = inject(Router);
+
+  if (tokenStorage.isLoggedIn()) {
+    return true;
+  }
+
+  router. navigate(['/login']);
+  return false;
 };
