4f313d9f078c21291f01821288a69f4e260cd9fa
JOEL-BINU-PHILIP <joelbinuphilip@gmail.com>
2025-12-14 23:49:45 +0530

    Implemented Add Airline Component

--- src/app/components/add-airline/add-airline.component.ts
+++ src/app/components/add-airline/add-airline.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { FlightService } from '../../services/flight.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-airline',
-  imports: [],
-  templateUrl: './add-airline.html',
-  styleUrl: './add-airline.css',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './add-airline.component.html',
+  styleUrl: './add-airline.component.css'
 })
-export class AddAirline {
+export class AddAirlineComponent {
+  form: any = {
+    name:  '',
+    logoUrl: ''
+  };
 
+  isSuccessful = false;
+  isFailed = false;
+  errorMessage = '';
+  isLoading = false;
+
+  constructor(private flightService: FlightService) {}
+
+  onSubmit(): void {
+    this.isLoading = true;
+    this.isFailed = false;
+    this.isSuccessful = false;
+
+    this.flightService.addAirline(this.form).subscribe({
+      next: data => {
+        this.isSuccessful = true;
+        this.isLoading = false;
+        this.form = { name: '', logoUrl: '' };
+      },
+      error: err => {
+        this.errorMessage = err.error || 'Failed to add airline';
+        this.isFailed = true;
+        this.isLoading = false;
+      }
+    });
+  }
 }
