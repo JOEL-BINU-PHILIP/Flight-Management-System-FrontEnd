393bfb52cf81328d0330ab20aeaaa381692d7f6a
JOEL-BINU-PHILIP <joelbinuphilip@gmail.com>
2025-12-20 09:44:09 +0530

    Initialised view-ticket and booking history component

---
+++ src/app/components/view-ticket/view-ticket.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-ticket',
+  imports: [],
+  templateUrl: './view-ticket.html',
+  styleUrl: './view-ticket.css',
+})
+export class ViewTicket {
+
+}
